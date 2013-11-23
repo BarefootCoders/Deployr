696ca08262e69912b4c25a8278c3d07d8d52c3e7
Jason Berlinsky <jason@barefootcoders.com>
2013-11-23 23:56:23 +0200

    Accept Heroku credentials

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,11 @@
 class Project < ActiveRecord::Base
-  attr_accessible :deploy_public_key, :github_repo, :deploy_repo, :user_id, :travis_token, :github_token
+  attr_accessible :deploy_public_key, :github_repo, :deploy_repo, :user_id, :travis_token, :github_token, :heroku_token
   validates_uniqueness_of :github_repo
 
   has_many :builds, dependent: :destroy
 
+  after_create :add_key_to_github!
+  after_create :add_key_to_heroku!
   after_create :fetch_builds!
 
   def fetch_builds!
@@ -20,6 +22,20 @@ class Project < ActiveRecord::Base
   def travis_project
     Travis.access_token = self.travis_token
     Travis::Repository.find(github_repo.gsub('git@github.com:', ''))
+  end
+
+  def add_key_to_github!
+    begin
+      github = Github.new(oauth_token: github_token)
+      user, repo = github_repo.split("/")
+      github.repos.keys.create(user, repo, title: "deployr", key: deploy_public_key)
+    rescue ; end
+  end
+
+  def add_key_to_heroku!
+    heroku = Heroku::API.new(api_key: heroku_token)
+    heroku.post_key(self.deploy_public_key)
+    heroku.post_key()
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,6 @@ project = Project.create!(
   deploy_repo: "git@heroku.com:deployr.git",
   deploy_public_key: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCpJ4PMTzM34eavYl86Jtm7Cp7+3k72vQum4FZO4F/iO0cC5GXKuIECU3t5bG0+bL99lTz7Evn51DSAWwJwDY0pLoUhYR/qRwKgh7xYsqAHypj0lEtvrigNPIq8RWSAayQkfGUaojc77usxl+7ntiLBnOlHPgH6TcyuhIVBbsLlmwiFgneHm8fgAABSDHfJkM8fBIljXXIfmvPLYnFbE36WHPzFpDc92fKettPzocE8Ez/JrtbfaHsctDiYcMtPPopBsq3maQ/haB9DAnzLInDje6fONR/LdEsfT10NK3rkoT2nqgmyTEKlh9+u/fiaJZ9osSe8gAQKnCdnmBKGLwLd jasonberlinsky@Jasons-MacBook-Pro.local",
   travis_token: "lozskpAZMjVIZ-SgUoWTyw",
-  github_token: "f9e6004a3353571a6fbe6d3f2f2d024027b76350"
+  github_token: "f9e6004a3353571a6fbe6d3f2f2d024027b76350",
+  heroku_token: "d57db5d2-d25c-4509-a753-dc2093cd2d8a"
 )
