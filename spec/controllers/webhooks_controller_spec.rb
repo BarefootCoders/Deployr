6c1864504cfb28088a27a924491a4b8d06e2665c
Jason Berlinsky <jason@barefootcoders.com>
2013-11-23 20:15:14 +0200

    Mark webhooks/travis pending

--- spec/controllers/webhooks_controller_spec.rb
+++ spec/controllers/webhooks_controller_spec.rb
@@ -3,10 +3,7 @@ require 'spec_helper'
 describe WebhooksController do
 
   describe "GET 'travis'" do
-    it "returns http success" do
-      get 'travis'
-      response.should be_success
-    end
+    pending
   end
 
 end
